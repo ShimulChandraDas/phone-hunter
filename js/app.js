c276a445696b27ba1e1788aca32508a90f4f0234
ShimulChandraDas <msshimul99@gmail.com>
2022-02-28 20:58:03 +0600

    search handel done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-alert('welcome')
+const allPhone = () => {
+    const searchValue = document.getElementById('search-field').value;
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+    fetch(url)
+        .then(rep => rep.json())
+        .then((data) => console.log(data));
+
+    //console.log(url);
+};
