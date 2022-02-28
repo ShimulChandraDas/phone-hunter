6fb1829b83c8e3ec4689cdb29385156879dcba50
ShimulChandraDas <msshimul99@gmail.com>
2022-02-28 21:58:30 +0600

    display set

--- js/app.js
+++ js/app.js
@@ -1,10 +1,29 @@
+//search handeling
 const allPhone = () => {
     const searchValue = document.getElementById('search-field').value;
 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
     fetch(url)
         .then(rep => rep.json())
-        .then((data) => console.log(data));
+        .then((data) => displayPhoneDetails(data.data));
 
     //console.log(url);
 };
+const displayPhoneDetails = (phones) => {
+
+    for (const phone of phones) {
+        const parent = document.getElementById('phone-container');
+        const div = document.createElement('div');
+        div.innerHTML = ` <div 
+            class=" card p-3  border bg-light">
+         <div class="pro-pic">
+             <img class="w=25" src="" alt="">
+         </div>
+            <h2>Name:</h2>
+            <button class="btn btn-success details-btn">Show Details</button>
+         </div>`;
+        parent.appendChild(div);
+    }
+
+    console.log(phones);
+}
