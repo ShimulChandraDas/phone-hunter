4765d9907329d7114b06852c9aa03f75534546ba
ShimulChandraDas <msshimul99@gmail.com>
2022-03-01 11:16:39 +0600

    disply details

--- js/app.js
+++ js/app.js
@@ -5,11 +5,11 @@ const allPhone = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
     fetch(url)
         .then(rep => rep.json())
-        .then((data) => displayPhoneDetails(data.data));
+        .then((data) => displayPhone(data.data));
 
     //console.log(url);
 };
-const displayPhoneDetails = (phones) => {
+const displayPhone = (phones) => {
 
     for (const phone of phones) {
         const parent = document.getElementById('phone-container');
@@ -21,7 +21,7 @@ const displayPhoneDetails = (phones) => {
              <img class="w=25" src="${phone.image}" alt="">
          </div>
             <h4>${phone.phone_name}</h4>
-            <button onclick="details('helo')" class="btn btn-success details-btn">Show Details</button>
+            <button onclick="details('${phone.slug}')" class="btn btn-success details-btn">Show Details</button>
          </div>`;
         parent.appendChild(div);
     }
@@ -29,6 +29,34 @@ const displayPhoneDetails = (phones) => {
     //console.log(phone);
 };
 
-const details = (info) => {
-    console.log('kiked', info)
+const details = (id) => {
+    const url = ` https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+        .then(rep => rep.json())
+        .then((data) => displayPhoneDetails(data.data));
+
+    //console.log(url)
+};
+
+const displayPhoneDetails = (info) => {
+
+    document.getElementById('details-container').innerHTML = `
+    <div>
+    <h1>${info.name}</h1>
+    <img class="w=25" src="${info.image}" alt="">
+    <h3>${info.slug}</h3>
+    <h4>${info.mainFeatures.storage}</h4>
+
+
+
+    <h6> ChipSet: ${info.mainFeatures.chipSet}</h6>
+    <h6> Display Size:  ${info.mainFeatures.displaySize}</h6>
+    <h6> Memory: ${info.mainFeatures.memory}</h6>
+    <span>${info.mainFeatures.sensors[0]}</span>, <span>${info.mainFeatures.sensors[1]}</span>, <span>${info.mainFeatures.sensors[2]}</span>, <span>${info.mainFeatures.sensors[3]}</span>,<span>${info.mainFeatures.sensors[4]}</span>,<span>${info.mainFeatures.sensors[5]}</span>,<span>${info.mainFeatures.sensors[6]}</span>
+
+    </div>
+    `
+    console.log(info)
+
+
 }
